3657244039c3a995aeba7495a51af04f7a19d3c6
72nd <msg@frg72.com>
2020-11-25 12:56:29 +0100

    new public folder, index page

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"os"
 
 	"github.com/sirupsen/logrus"
--- src/web.go
+++ src/web.go
@@ -2,9 +2,25 @@ package ttrn
 
 import (
 	"fmt"
+	"html/template"
+	"io"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
+// TemplateRenderer implements the Echo renderer interface.
+type TemplateRenderer struct {
+	tpls *template.Template
+}
+
+// Implement the Echo renderer interface.
+func (t TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+	return t.tpls.ExecuteTemplate(w, name, data)
+}
 
 // Web handles the web view stuff.
 type Web struct {
@@ -14,13 +30,29 @@ type Web struct {
 
 // NewWeb returns a new instance of the Web struct.
 func NewWeb(cfg Config) Web {
+	e := echo.New()
+	e.Renderer = &TemplateRenderer{
+		tpls: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	addRoutes(e, cfg)
+
 	return Web{
 		cfg:  cfg,
-		echo: echo.New(),
+		echo: e,
 	}
 }
 
 // Run runs the web server.
-func (w Web) Run() {
+func (w *Web) Run() {
 	w.echo.Logger.Fatal(w.echo.Start(fmt.Sprintf(":%d", w.cfg.Port)))
+}
+
+// addRoutes adds the routes to the echo element.
+func addRoutes(e *echo.Echo, cfg Config) {
+	e.GET("/", getIndex)
+}
+
+// getIndex handles the GET request on /. 
+func getIndex(c echo.Context) error {
+	return c.Render(http.StatusOK, "index.html", map[string]interface{}{})
 }
